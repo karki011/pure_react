cbe4e33451007b2a5e7328e1fa27f9bae510de6b
Subash karki <karki011@gmail.com>
2018-06-07 19:46:56 -0500

    jsx

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function MyThings(){
-
+function MyThings() {
+    return (
+    <div className='book'> 
+        <div className='title'>
+          The Title
+        </div>
+        <div className='author'>
+          The Author
+        </div>
+        <ul className='stats'>
+            <li className='rating'> 5 stars
+            </li>
+            <li className='isbn'>
+                  12-345678-910
+            </li> 
+        </ul>
+    </div>
+    )
 }
 
 
